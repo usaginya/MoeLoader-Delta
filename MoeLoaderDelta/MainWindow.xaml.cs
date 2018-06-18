bc150136ce3905a54f404157b3bae35fc1a509b5
YIU <usaginya@vip.qq.com>
2018-06-18 14:21:58 +0800

    update to version 7.0.5.4
    1. 修正 未设定命名格式时重名bug、使用原名作为文件名
    2. 更改 不支持的格式预览直接打开详情页面

--- MoeLoaderDelta/MainWindow.xaml.cs
+++ MoeLoaderDelta/MainWindow.xaml.cs
@@ -802,7 +802,7 @@ namespace MoeLoaderDelta
                 {
                     imgs[index].ImgP = c + 1 + "";
                 }
-                string fileName = GenFileName(dlimg);
+                string fileName = GenFileName(dlimg, oriUrls[c]);
                 string domain = SiteManager.Instance.Sites[nowSelectedIndex].ShortName;
                 downloadC.AddDownload(new MiniDownloadItem[] {
                     new MiniDownloadItem(fileName, oriUrls[c], domain, dlimg.Author, "", "", dlimg.Id, dlimg.NoVerify)
@@ -1446,13 +1446,17 @@ namespace MoeLoaderDelta
         {
             int index = (int)sender;
 
-            //排除不支持预览的格式
+            //不支持预览的格式使用浏览来源页
             string supportformat = "jpg jpeg png bmp gif";
             string ext = BooruProcessor.FormattedImgUrl("", imgs[index].PreviewUrl.Substring(imgs[index].PreviewUrl.LastIndexOf('.') + 1));
             if (!supportformat.Contains(ext))
             {
-                MessageBox.Show(this, "未支持" + ext + "格式的预览显示，请下载后使用其它程序方式打开文件预览",
-                    ProgramName, MessageBoxButton.OK, MessageBoxImage.Warning);
+                try
+                {
+                    if (imgs[index].DetailUrl.Length > 0)
+                        System.Diagnostics.Process.Start(imgs[index].DetailUrl);
+                }
+                catch (Exception) { }
                 return;
             }
 
@@ -1728,7 +1732,7 @@ namespace MoeLoaderDelta
 
                                 //url|文件名|域名|上传者|ID(用于判断重复)
                                 text += oriUrls[c]
-                                    + "|" + GenFileName(selectimg)
+                                    + "|" + GenFileName(selectimg, oriUrls[c])
                                     + "|" + host
                                     + "|" + selectimg.Author
                                     + "|" + selectimg.Id
@@ -2018,7 +2022,7 @@ namespace MoeLoaderDelta
                         {
                             imgs[i].ImgP = c + 1 + "";
                         }
-                        string fileName = GenFileName(dlimg);
+                        string fileName = GenFileName(dlimg, oriUrls[c]);
                         string domain = SiteManager.Instance.Sites[nowSelectedIndex].ShortName;
                         urls.Add(new MiniDownloadItem(fileName, oriUrls[c], domain, dlimg.Author, "", "", dlimg.Id, dlimg.NoVerify));
                     }
@@ -2032,11 +2036,14 @@ namespace MoeLoaderDelta
         /// 构建文件名 generate file name
         /// </summary>
         /// <param name="img"></param>
+        /// <param name="url">下载链接用于提取原名</param>
         /// <returns></returns>
-        private string GenFileName(Img img)
+        private string GenFileName(Img img,string url)
         {
             //namePatter
             string file = namePatter;
+            if (string.IsNullOrWhiteSpace(file))
+                return System.IO.Path.GetFileName(url);
 
             //%site站点 %id编号 %tag标签 %desc描述 %author作者 %date图片时间 %imgid[2]图册中图片编号[补n个零]
             file = file.Replace("%site", SiteManager.Instance.Sites[nowSelectedIndex].ShortName);
